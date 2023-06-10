47ec35f995332885c6906b4c63df936ffcc1c87e
Wgz <1808365012@mail.ynu.edu.cn>
2023-06-10 15:59:28 +0800

    根据openapi,提供文档，生成前端调用后端的方法

--- ttb_api_frontend/config/config.ts
+++ ttb_api_frontend/config/config.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routes from './routes';
+
 const { REACT_APP_ENV } = process.env;
 export default defineConfig({
   hash: true,
@@ -48,15 +49,8 @@ export default defineConfig({
   openAPI: [
     {
       requestLibPath: "import { request } from 'umi'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      schemaPath: join(__dirname, 'oneapi.json'),
-      mock: false,
-    },
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-      projectName: 'swagger',
+      schemaPath: 'http://localhost:7529/api/v3/api-docs',
+      projectName: 'ttb_api_backend',
     },
   ],
   nodeModulesTransform: {
