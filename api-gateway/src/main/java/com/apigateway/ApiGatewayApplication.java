79b23aff582feeca69d96c7a030a6a7eb8a738de
Wgz <1808365012@mail.ynu.edu.cn>
2023-08-16 16:46:28 +0800

    添加网关

---
+++ api-gateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 下水道的小老鼠
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/bilibili")
+                        .uri("https://www.bilibili.com"))
+                .route("host_route", r -> r.path("/baidu")
+                        .uri("https://www.baidu.com"))
+                .build();
+    }
+}
